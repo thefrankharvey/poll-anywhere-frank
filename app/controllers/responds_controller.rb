5765417f87a0154fa851ad19d3197daaf2254316
Frank <frank@thefrankharvey.com>
2022-10-03 09:10:39 -0400

    sets up responds view and controller - renders options

--- app/controllers/responds_controller.rb
+++ app/controllers/responds_controller.rb
@@ -1,8 +1,12 @@
 class RespondsController < ApplicationController
-  before_action :set_poll
-# todo focus here
+  before_action :set_poll, :set_options
+
   private
     def set_poll
       @poll = Poll.find(params[:poll_id])
+    end
+
+    def set_options
+      @poll_options = Option.where(params[:poll_id])
     end
 end
